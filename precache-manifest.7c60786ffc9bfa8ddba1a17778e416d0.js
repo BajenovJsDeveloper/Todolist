d07ae9ab2cbbca0740948e9180b839f2131fed8e
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-08-18 00:55:05 +0300

    Updates

---
+++ precache-manifest.7c60786ffc9bfa8ddba1a17778e416d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f2e7a062ba2349c0e065fa128e6904",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "46b843d231d08861f194",
+    "url": "/todolist/static/css/main.1c831f4c.chunk.css"
+  },
+  {
+    "revision": "fd8891854d074c103b4d",
+    "url": "/todolist/static/js/2.63fff441.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.63fff441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b843d231d08861f194",
+    "url": "/todolist/static/js/main.3e919cce.chunk.js"
+  },
+  {
+    "revision": "7558fb00b4542e238607",
+    "url": "/todolist/static/js/runtime-main.97699757.js"
+  }
+]);
